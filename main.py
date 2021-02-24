188c063663ace6856d8ce52723cdf81df315dd0a
atrox3d <atrox3d@gmail.com>
2021-02-24 13:36:28 +0100

    add API call exclusions, check for rain

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ KUNITS = "units"
 KLATITUDE = "lat"
 KLONGITUDE = "lon"
 KAPI_KEY = "appid"
+KEXCLUDE = "exclude"
 
 
-def call_weather_api(endpoint: str, params: dict):
+def call_weather_api(endpoint: str, params: dict) -> (int, dict):
+    print(f"calling endpoint: {endpoint}")
+    print(f"with params: {params}")
     response = requests.get(endpoint, params)
     response.raise_for_status()
     return response.status_code, response.json()
 
 
-def get_current_weather():
+def get_current_weather() -> (int, dict):
     params = {
         KCITY: f"{private.CITY_IT},{private.COUNTRY_CODE}",
         KLANGUAGE: private.LANGUAGE,
@@ -32,17 +35,47 @@ def get_current_weather():
     return call_weather_api(CURRENT_WEATHER_ENDPOINT, params)
 
 
-def get_onecall_forecast():
+def get_onecall_forecast(current=True, minutely=True, hourly=True, daily=True) -> (int, dict):
+    # https://www.ventusky.com/merauke
+    rainy_latitude = -8.484026790361654
+    rainy_longitude = 140.40224668902974
     params = {
-        KLATITUDE: private.LATITUDE,
-        KLONGITUDE: private.LONGITUDE,
+        # KLATITUDE: private.LATITUDE,
+        # KLONGITUDE: private.LONGITUDE,
+        KLATITUDE: rainy_latitude,
+        KLONGITUDE: rainy_longitude,
         KLANGUAGE: private.LANGUAGE,
         KAPI_KEY: private.API_KEY
     }
+
+    exclude = []
+    if not current:
+        exclude.append("current")
+    if not minutely:
+        exclude.append("minutely")
+    if not hourly:
+        exclude.append("hourly")
+    if not daily:
+        exclude.append("daily")
+
+    if len(exclude):
+        exclude_param = ",".join(exclude)
+        params["exclude"] = exclude_param
+
     return call_weather_api(ONE_CALL_ENDPOINT, params)
 
 
-statuscode, jsondata = get_onecall_forecast()
+# get only hourly
+statuscode, weather_data = get_onecall_forecast(current=False, minutely=False, daily=False)
 print(statuscode)
-hourly = jsondata["hourly"]
-print(json.dumps(hourly, indent=4))
+# print(json.dumps(weather_data, indent=4))
+
+umbrella = False
+for hour in range(20):
+    id = weather_data["hourly"][hour]["weather"][0]["id"]
+    print(id)
+    if id < 700:
+        umbrella = True
+
+if umbrella:
+    print("RAIN!")
